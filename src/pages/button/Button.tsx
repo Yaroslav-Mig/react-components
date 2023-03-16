e231917ff8a9f040d03c6c863316a31dc5c88801
Yaroslav Mescheryakov <migyaroslav@gmail.com>
2023-03-16 16:24:49 +0300

    refact: add different buttons types

--- src/pages/button/Button.tsx
+++ src/pages/button/Button.tsx
@@ -7,24 +7,39 @@ type DefaultButtonPropsType = DetailedHTMLProps<
   HTMLButtonElement
 >;
 
-type colorStr = 'default' | 'primary' | 'secondary';
+type btnColor = 'default' | 'primary' | 'secondary';
 
 type SuperButtonProps = DefaultButtonPropsType & {
-  btnType?: colorStr;
+  btnType?: btnColor;
+	isActive?: boolean;
 };
 
 const Button: FC<SuperButtonProps> = (props) => {
-  const { onClick, className, btnType = 'default', disabled, ...attr } = props;
-  const { btn } = css;
+  const {
+    onClick,
+    className,
+    btnType = className ? undefined : 'default',
+    disabled = false,
+		isActive = false,
+    ...attrs
+  } = props;
 
-  const styles = clsx(btn, className, { [css[`${btnType}`]]: btnType });
+  const { btn, btn_disabled } = css;
+
+  const classes = clsx(btn, {
+		[css[`${btnType}`]]: btnType,
+		[css[`${className}`]]:className,
+		[css[`btn__${btnType}_active`]]: isActive,
+    [btn_disabled]: disabled,
+  });
 
   const clickAction = (e: MouseEvent<HTMLButtonElement>): void => {
     e.preventDefault();
     onClick && onClick(e);
-  };
+	};
 
-  return <button className={styles} onClick={clickAction} disabled={disabled} {...attr} />;
+
+  return <button className={classes} onClick={clickAction} disabled={disabled} {...attrs} />;
 };
 
 export default Button;
--- src/pages/button/ButtonStand.tsx
+++ src/pages/button/ButtonStand.tsx
@@ -1,24 +1,68 @@
-import React from 'react'
-import Button from './Button'
+import Button from './Button';
+import css from './Button.module.css';
 
 const ButtonStand = () => {
-	return (
-		<>
-			<div>
-				<h3>1. Different buttons, hover effect and onClick function:</h3>
-				<Button onClick={() => console.log('I`m button')}>
-					Button default
-				</Button>
-				<Button onClick={() => console.log('I`m button')} btnType='primary'>
-					Button primary
-				</Button>
-				<Button onClick={() => console.log('I`m button')} btnType='secondary'>
-					Button secondary
-				</Button>
-			</div>
+  return (
+    <>
+      <div>
+        <h3>1. Default buttons, hover effect, active and onClick function:</h3>
+        <div className={css.box}>
+          <Button onClick={() => console.log('I`m button')}>Button default</Button>
+          <Button onClick={() => console.log('I`m button')} btnType='primary'>
+            Button primary
+          </Button>
+          <Button onClick={() => console.log('I`m button')} btnType='secondary'>
+            Button secondary
+          </Button>
+        </div>
+      </div>
 
-		</>
-	)
-}
+      <div>
+        <h3>2. Active buttons and hover effect:</h3>
+        <div className={css.box}>
+          <Button isActive>Button active</Button>
+          <Button btnType='primary' isActive>
+            Button active
+          </Button>
+          <Button btnType='secondary' isActive>
+            Button active
+          </Button>
+        </div>
+      </div>
 
-export default ButtonStand
+      <div>
+        <h3>3. Disabled buttons:</h3>
+        <div className={css.box}>
+          <Button disabled>Button disabled</Button>
+          <Button btnType='primary' disabled>
+            Button disabled
+          </Button>
+          <Button btnType='secondary' disabled>
+            Button disabled
+          </Button>
+        </div>
+      </div>
+
+      <div>
+        <h3>4. Supporting data, type and etc. attributes</h3>
+        <div className={css.box}>
+          <Button data-name='button' btnType='primary'>
+            Button disabled
+          </Button>
+          <Button type='submit' btnType='secondary'>
+            Button disabled
+          </Button>
+        </div>
+      </div>
+
+      <div>
+        <h3>5. Private class:</h3>
+        <div className={css.box}>
+          <Button className='outline'>Button</Button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ButtonStand;
